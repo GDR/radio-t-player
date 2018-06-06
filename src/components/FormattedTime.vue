8c5282023a13848eb638935a4c9fa3270634540d
Damir Garifullin <gosugdr@gmail.com>
2018-06-06 23:51:18 +0300

    Added icons

---
+++ src/components/FormattedTime.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    {{hh}}:{{mm}}:{{ss}}
+  </span>
+</template>
+
+<script>
+
+const format = (num) =>  {
+  if (num < 10) {
+    return '0' + parseInt(num);
+  }
+  return parseInt(num)
+}
+
+export default {
+  name: "FormattedTime",
+  props: {
+    time: Number
+  },
+  computed: {
+    hh() {
+      return format(this.time / (60 * 60))
+    },
+    mm() {
+      return format(parseInt(this.time / 60) - this.hh * 60)
+    },
+    ss() {
+      return format(this.time % 60)
+    }
+  }
+}
+</script>
+
