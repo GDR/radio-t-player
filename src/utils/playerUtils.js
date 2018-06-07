f97f1ebebd45cdc4dd57bbde1f786d56ae5db3a6
Damir Garifullin <gosugdr@gmail.com>
2018-06-07 23:39:12 +0300

    Refactored seconds formatting

--- src/utils/playerUtils.js
+++ src/utils/playerUtils.js
@@ -1,4 +1,11 @@
+import moment from 'moment';
 
 export const getClickedPercent = (offsetX, width) => {
   return Math.min(Math.max((offsetX / width * 100), 0), 100);
+};
+
+export const formatSecondsToTime = (seconds) => {
+  return moment().startOf('day')
+    .seconds(seconds)
+    .format('HH:mm:ss')
 };
